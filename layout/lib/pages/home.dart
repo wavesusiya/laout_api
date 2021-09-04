c41b074e81a4c86fddf859095ee212b3b12e38b2
wavesusiya <90094216+wavesusiya@users.noreply.github.com>
2021-09-04 21:00:44 +0700

    update function api

--- layout/lib/pages/home.dart
+++ layout/lib/pages/home.dart
@@ -2,6 +2,8 @@ import 'dart:convert';
 
 import 'package:flutter/material.dart';
 import 'package:layout/pages/detail_page.dart';
+import 'package:http/http.dart' as http;
+import 'dart:async';
 
 class HomePage extends StatefulWidget {
   //const HomePage({ Key? key }) : super(key: key);
@@ -20,18 +22,22 @@ class _HomePageState extends State<HomePage> {
         body: Padding(
             padding: const EdgeInsets.all(10),
             child: FutureBuilder(
-              builder: (context, snapshot) {
-                var data = json.decode(snapshot.data.toString());
+              builder: (context, AsyncSnapshot snapshot) {
+                //var data = json.decode(snapshot.data.toString());
                 return ListView.builder(
                   itemBuilder: (BuildContext context, int index) {
-                    return MyBox(data[index]['title'], data[index]['subtitle'],
-                        data[index]['imges'], data[index]['detail']);
+                    return MyBox(
+                        snapshot.data[index]['title'],
+                        snapshot.data[index]['subtitle'],
+                        snapshot.data[index]['imges'],
+                        snapshot.data[index]['detail']);
                   },
-                  itemCount: data.length,
+                  itemCount: snapshot.data.length,
                 );
               },
-              future:
-                  DefaultAssetBundle.of(context).loadString('assets/data.json'),
+              future: getData(),
+              // future:
+              //     DefaultAssetBundle.of(context).loadString('assets/data.json'),
             )),
         bottomNavigationBar: BottomNavigationBar(
           //currentIndex: _currentInex,
@@ -98,5 +104,14 @@ class _HomePageState extends State<HomePage> {
         ],
       ),
     );
+  }
+
+  Future getData() async {
+    //https://raw.githubusercontent.com/wavesusiya/laout_api/main/data.json
+    var url = Uri.https(
+        'raw.githubusercontent.com', '/wavesusiya/laout_api/main/data.json');
+    var response = await http.get(url);
+    var result = json.decode(response.body);
+    return result;
   }
 }
